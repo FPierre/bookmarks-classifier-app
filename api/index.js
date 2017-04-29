56a585a2886e44a7f18361187bcebc616b5e639d
FPierre <pflauder@gmail.com>
2017-04-29 17:52:51 +0200

    Fonction de suppression des stopwords sur une string

--- api/index.js
+++ api/index.js
@@ -1,16 +1,54 @@
-const ap = require('express').express()
+// const express = require('express')
+const stopWords = require('stopwords-json')
 
-const rubyTrainer = require('./trainers/ruby-trainer')
-const javaScriptTrainer = require('./trainers/javascript-trainer')
+// const app = express.express()
 
-const trainers = rubyTrainer.concat(javaScriptTrainer)
+// console.log(stopWords['fr'])
+// console.log(stopWords['en'])
 
-app.get('/trainers', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+// const rubyTrainer = require('./trainers/ruby-trainer')
+// const javaScriptTrainer = require('./trainers/javascript-trainer')
 
-  res.json({ trainers: trainers })
-})
+// const trainers = rubyTrainer.concat(javaScriptTrainer)
 
-app.listen(3003)
+// TODO: remove stop words here
+
+// app.get('/trainers', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET')
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+//
+//   res.json({ trainers: trainers })
+// })
+//
+// app.listen(3003)
+
+String.prototype.removeStopWords () {
+  const originalString = this.valueOf()
+
+  // Split all the individual words in the phrase
+  const words = originalString.match(/[^\s]+|\s+[^\s+]$/g)
+
+  for (let x = 0; x < words.length; x++) {
+    for (let y = 0; y < stopWords.length; y++) {
+      // Trim, remove non-alpha and lower case
+      const word = words[x].replace(/\s+|[^a-z]+/ig, '').toLowerCase()
+      const stopWord = stopWords[y]
+
+      // If the word matches the stop word, remove it from the keywords
+      if (word === stopWord) {
+        // Build the regex
+        let regexStr = '^\\s*' + stopWord + '\\s*$' // Only word
+        regexStr += '|^\\s*' + stopWord + '\\s+'    // First word
+        regexStr += '|\\s+' + stopWord + '\\s*$'    // Last word
+        regexStr += '|\\s+' + stopWord + '\\s+'     // Word somewhere in the middle
+        regex = new RegExp(regexStr, 'ig')
+
+        // Remove the word from the keywords
+        originalString = originalString.replace(regex, ' ')
+      }
+    }
+  }
+
+  return originalString.replace(/^\s+|\s+$/g, '')
+}

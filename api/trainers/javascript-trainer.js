5a178f8c48e4bdd14b9aa961e7c836a43b25bc73
FPierre <pflauder@gmail.com>
2017-05-04 21:40:48 +0200

    Ajout de la langue du titre dans les trainers

--- api/trainers/javascript-trainer.js
+++ api/trainers/javascript-trainer.js
@@ -1,7 +1,7 @@
 module.exports = [
-  { tag: 'javascript', text: 'Chart.js | Open source HTML5 Charts for your website' },
-  { tag: 'javascript', text: 'Getting Started with Webpack: Module Bundling Magic' },
-  { tag: 'javascript', text: 'Intro to Webpack' },
-  { tag: 'javascript', text: 'Overview of JavaScript ES6 features (a.k.a ECMAScript 6 and ES2015+)' },
-  { tag: 'javascript', text: 'Things you should know about JS events' }
+  { tag: 'javascript', lang: 'en', text: 'Chart.js | Open source HTML5 Charts for your website' },
+  { tag: 'javascript', lang: 'en', text: 'Getting Started with Webpack: Module Bundling Magic' },
+  { tag: 'javascript', lang: 'en', text: 'Intro to Webpack' },
+  { tag: 'javascript', lang: 'en', text: 'Overview of JavaScript ES6 features (a.k.a ECMAScript 6 and ES2015+)' },
+  { tag: 'javascript', lang: 'en', text: 'Things you should know about JS events' }
 ]
--- api/trainers/ruby-trainer.js
+++ api/trainers/ruby-trainer.js
@@ -1,9 +1,9 @@
 module.exports = [
-  { tag: 'ruby', text: 'Please, Ruby devs, join() your paths' },
-  { tag: 'ruby', text: 'Augmenting A Ruby on Rails App with Vue.js' },
-  { tag: 'ruby', text: 'Rails isn’t trendy anymore. Hooray for Rails!' },
-  { tag: 'ruby', text: 'Incremental Garbage Collection in Ruby 2.2' },
-  { tag: 'ruby', text: 'A new way to understand your Rails app’s performance' },
-  { tag: 'ruby', text: 'Écrire un jeu en 2d avec Ruby et Gosu' },
-  { tag: 'ruby', text: 'Trends in Ruby - analyzing rubygems stats for 2015' }
+  { tag: 'ruby', lang: 'en', text: 'Please, Ruby devs, join() your paths' },
+  { tag: 'ruby', lang: 'en', text: 'Augmenting A Ruby on Rails App with Vue.js' },
+  { tag: 'ruby', lang: 'en', text: 'Rails isn’t trendy anymore. Hooray for Rails!' },
+  { tag: 'ruby', lang: 'en', text: 'Incremental Garbage Collection in Ruby 2.2' },
+  { tag: 'ruby', lang: 'en', text: 'A new way to understand your Rails app’s performance' },
+  { tag: 'ruby', lang: 'fr', text: 'Écrire un jeu en 2d avec Ruby et Gosu' },
+  { tag: 'ruby', lang: 'en', text: 'Trends in Ruby - analyzing rubygems stats for 2015' }
 ]

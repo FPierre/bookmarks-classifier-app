19ab56720d188c3c992a4e77d6c359222690a05d
FPierre <pflauder@gmail.com>
2017-05-04 21:20:19 +0200

    Suppression des noms des sites du trainer Ruby

--- api/trainers/ruby-trainer.js
+++ api/trainers/ruby-trainer.js
@@ -1,9 +1,9 @@
 module.exports = [
   { tag: 'ruby', text: 'Please, Ruby devs, join() your paths' },
   { tag: 'ruby', text: 'Augmenting A Ruby on Rails App with Vue.js' },
-  { tag: 'ruby', text: 'Rails isn’t trendy anymore. Hooray for Rails! | Ruby Fleebie' },
+  { tag: 'ruby', text: 'Rails isn’t trendy anymore. Hooray for Rails!' },
   { tag: 'ruby', text: 'Incremental Garbage Collection in Ruby 2.2' },
-  { tag: 'ruby', text: 'A new way to understand your Rails app’s performance - Weissblog' },
-  { tag: 'ruby', text: 'Écrire un jeu en 2d avec Ruby et Gosu - partie 1 - Xavier Nayrac' },
+  { tag: 'ruby', text: 'A new way to understand your Rails app’s performance' },
+  { tag: 'ruby', text: 'Écrire un jeu en 2d avec Ruby et Gosu' },
   { tag: 'ruby', text: 'Trends in Ruby - analyzing rubygems stats for 2015' }
 ]

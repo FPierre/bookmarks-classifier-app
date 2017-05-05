08ff8786d7f20a29f79532cc0a32f653f1dd5cf0
FPierre <pflauder@gmail.com>
2017-05-05 20:57:10 +0200

    Ajout de trainers

--- api/trainers/javascript-trainer.js
+++ api/trainers/javascript-trainer.js
@@ -3,5 +3,11 @@ module.exports = [
   { tag: 'javascript', lang: 'en', text: 'Getting Started with Webpack: Module Bundling Magic' },
   { tag: 'javascript', lang: 'en', text: 'Intro to Webpack' },
   { tag: 'javascript', lang: 'en', text: 'Overview of JavaScript ES6 features (a.k.a ECMAScript 6 and ES2015+)' },
-  { tag: 'javascript', lang: 'en', text: 'Things you should know about JS events' }
+  { tag: 'javascript', lang: 'en', text: 'Things you should know about JS events' },
+  { tag: 'javascript', lang: 'en', text: 'Electron is flash for the desktop' },
+  { tag: 'javascript', lang: 'en', text: 'Working with fonts with Webpack' },
+  { tag: 'javascript', lang: 'en', text: 'Integrating Vue.js and Socket.io' },
+  { tag: 'javascript', lang: 'en', text: 'Webpack: A Detailed Introduction' },
+  { tag: 'javascript', lang: 'en', text: 'Does Google execute JavaScript?' },
+  { tag: 'javascript', lang: 'en', text: 'Rethinking validations for Vue.js' },
 ]
---
+++ api/trainers/linux-trainer.js
@@ -0,0 +1,9 @@
+module.exports = [
+  { tag: 'linux', lang: 'en', text: 'My First 5 Minutes On A Server; Or, Essential Security for Linux Servers' },
+  { tag: 'linux', lang: 'en', text: '10 Awesome Examples for Viewing Huge Log Files in Unix' },
+  { tag: 'linux', lang: 'fr', text: 'TOP 10 des outils tip TOP sous Linux !' },
+  { tag: 'linux', lang: 'en', text: 'How To : Install Linux kernel 3.17.4 in Ubuntu/Linux Mint systems' },
+  { tag: 'linux', lang: 'en', text: 'Enabling and disabling services during start up in GNU/Linux' },
+  { tag: 'linux', lang: 'en', text: 'How to reset your password in Ubuntu' },
+  { tag: 'linux', lang: 'en', text: '10 Awesome Examples for Viewing Huge Log Files in Unix' }
+]

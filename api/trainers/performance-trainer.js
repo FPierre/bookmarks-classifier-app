08d9c639c98a039ba61ca300a131be1b71664894
FPierre <pflauder@gmail.com>
2017-05-05 22:18:38 +0200

    Ajout de trainer pour la catégorie performance

---
+++ api/trainers/performance-trainer.js
@@ -0,0 +1,11 @@
+module.exports = [
+  { tag: 'performance', lang: 'en', text: 'Webpack bits: Getting the most out of the CommonsChunkPlugin()' },
+  { tag: 'performance', lang: 'en', text: 'Make bundler crazy fast' },
+  { tag: 'performance', lang: 'en', text: 'Reducing the size of your Webpack bundle' },
+  { tag: 'performance', lang: 'en', text: 'How I built a super fast Uber clone for mobile web' },
+  { tag: 'performance', lang: 'en', text: 'A Six-Pronged Rails Performance Philosophy' },
+  { tag: 'performance', lang: 'en', text: 'High performance Rails' },
+  { tag: 'performance', lang: 'en', text: 'Tips for Writing Fast Rails' },
+  { tag: 'performance', lang: 'en', text: 'Is X or Y faster in Ruby?' },
+  { tag: 'performance', lang: 'en', text: 'A New Way to Understand Your Rails App’s Performance' }
+]

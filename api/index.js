55e4d4c108e71fb1ef8498418c9f283cf7016f34
FPierre <pflauder@gmail.com>
2017-05-06 09:24:55 +0200

    Amélioration de l'organisation du code

--- api/index.js
+++ api/index.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const rubyTrainer = require('./trainers/ruby-trainer')
+const javaScriptTrainer = require('./trainers/javascript-trainer')
 const { sanitizeTexts } = require('./utils')
 const bayes = require('./bayes')
 
 const app = express()
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const rubyTrainer = require('./trainers/ruby-trainer')
-const javaScriptTrainer = require('./trainers/javascript-trainer')
 const trainers = rubyTrainer.concat(javaScriptTrainer)
-
 const sanitizeTrainers = sanitizeTexts(trainers)
 
 for (let trainer of sanitizeTrainers) {
@@ -29,12 +27,12 @@ app.post('/guess', (req, res) => {
   res.json({ winner: bayes.winner() })
 })
 
-app.get('/trainers', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-
-  // res.json({ trainers:  })
-})
+// app.get('/trainers', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET')
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+//
+//   // res.json({ trainers:  })
+// })
 
 app.listen(3003)

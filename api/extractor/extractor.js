c392907ea17295307be09bb0418c84a5c7679be1
FPierre <pflauder@gmail.com>
2017-05-15 20:14:29 +0200

    Suppression de la clé correspondant au label dans le hash des pending

--- api/extractor/extractor.js
+++ api/extractor/extractor.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio')
 const franc = require('franc-min')
 
 const tags = ['ES6', 'JavaScript']
-const taggedBookmarks = {}
+const taggedBookmarks = []
 
 try {
   const content = fs.readFileSync('api/extractor/raindrop.io.html', 'utf8')
@@ -15,16 +15,12 @@ try {
     if (bookmarkTags) {
       const bookmarkTagsArray = bookmarkTags.split(',')
 
-      for (bookmarkTag of bookmarkTagsArray) {
+      for (const bookmarkTag of bookmarkTagsArray) {
         if (tags.includes(bookmarkTag)) {
-          if (!taggedBookmarks[bookmarkTag]) {
-            taggedBookmarks[bookmarkTag] = []
-          }
-
           const text = $(elem).text()
           const lang = franc(text) === 'fra' ? 'fra' : 'eng'
 
-          taggedBookmarks[bookmarkTag].push({
+          taggedBookmarks.push({
             tag: bookmarkTag,
             lang: lang,
             text: text
@@ -39,7 +35,7 @@ try {
 
 fs.writeFile('api/extractor/tagged-bookmarks.json', JSON.stringify(taggedBookmarks, undefined, 2), (err) => {
   if (err) {
-   return console.log(err)
+    return console.log(err)
   }
 
   console.log('The file was saved!')

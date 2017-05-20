be59c71e0dae3081240d7bfdf907e54a8338958a
FPierre <pflauder@gmail.com>
2017-05-20 13:22:51 +0200

    Ajout du package vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import VueResource from 'vue-resource'
 import VueTouch from 'vue-touch'
+import Vuex from 'vuex'
+import App from './App'
 
-Vue.use(VueResource).use(VueTouch)
+Vue.use(VueResource).use(VueTouch).use(Vuex)
 
 Vue.config.productionTip = false
 
